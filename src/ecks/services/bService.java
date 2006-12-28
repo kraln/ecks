cb18f404c932d7d979ba84a2c70afe91f160d380
TehKuja <TehKuja@2e3bee3b-6622-0410-b2a2-7f6c7436e678>
2006-12-28 06:59:08 +0000

    Code cleanup in aisle three!

--- src/ecks/services/bService.java
+++ src/ecks/services/bService.java
@@ -29,12 +29,13 @@ import org.w3c.dom.NodeList;
 
 public abstract class bService implements Service {
     public Map<String, CommandModule> Commands;
+
     public bService() {
         Commands = new HashMap<String, CommandModule>();
     }
 
     public void introduce() {
-        Generic.srvIntroduce( this);
+        Generic.srvIntroduce(this);
     }
 
     public abstract void setname(String nname);
@@ -44,38 +45,68 @@ public abstract class bService implements Service {
         command = command.trim();
         String cmd = command.split(" ")[0];
 
-        if (cmd.startsWith("FQDN"))
-        {
+        if (cmd.startsWith("FQDN")) {
             cmd = cmd.substring(4); // remove the fqdn
             command = command.substring(4);
             Logging.verbose("SERVICE", "Command \"" + cmd + "\" is fully qualified.");
         }
         try {
-        if (Commands.containsKey(cmd.toLowerCase())) {
+
+            // Do we have this command?
+            if (!Commands.containsKey(cmd.toLowerCase())) {
+                Generic.curProtocol.outPRVMSG(this, replyto, "\u0002Error:\u0002 Unknown Command!");
+                return;
+            }
+
             CommandDesc c = Commands.get(cmd.toLowerCase()).getDesc();
-            if ((inchan && c.InChannel) || !inchan)               // inchannel check
-            {
-                if (command.split(" ").length <= (c.ArgCount + 1)) // too many arguments check
-                {
-                    if (Configuration.getSvc().containsKey(Configuration.authservice)) // if we have an authserv
-                    {
-                        CommandDesc.access_levels req = Commands.get(cmd.toLowerCase()).getDesc().Required_Access;
-                        String handle = null;
-                        if (Generic.Users.containsKey(user.toLowerCase()))
-                            handle = Generic.Users.get(user.toLowerCase()).authhandle;
-                        CommandDesc.access_levels cur = ((SrvAuth) Configuration.getSvc().get(Configuration.authservice)).checkAccess(handle);
-                        if ((handle != null) || (req.ordinal() == 0)) { // we're authed, or the command doesn't care
-                            if (cur.ordinal() >= req.ordinal()) { // if we have the access
-                                Logging.verbose("SERVICE", "Handling command: " + cmd + ", for user: " + user + ".");
-                                Commands.get(cmd.toLowerCase()).handle_command(this, user.toLowerCase(), replyto, command.substring(cmd.length()).trim()); // run command
-                            } else Generic.curProtocol.outPRVMSG(this, replyto, "\u0002Error:\u0002 Not enough access!");
-                        } else Generic.curProtocol.outPRVMSG(this, replyto, "\u0002Error:\u0002 You're not authed!");
-                    } else Commands.get(cmd.toLowerCase()).handle_command(this, user, replyto, command.substring(cmd.length()).trim()); // just do it without checking levels
-                } else Generic.curProtocol.outPRVMSG(this, replyto, "\u0002Error:\u0002 Too many arguments!");
-            } else Generic.curProtocol.outPRVMSG(this, replyto, "\u0002Error:\u0002 That command is unavailable in-channel!");
-        } else Generic.curProtocol.outPRVMSG(this, replyto, "\u0002Error:\u0002 Unknown Command!");
-        } catch (NullPointerException NPE)
-        {
+            // Is this command available in-channel?
+            if ((!inchan || !c.InChannel) && inchan) {
+                Generic.curProtocol.outPRVMSG(this, replyto, "\u0002Error:\u0002 That command is unavailable in-channel!");
+                return;
+            }
+
+            // Do we have too many arguments?
+            if (command.split(" ").length > (c.ArgCount + 1)) {
+                // todo: accept commands with no limit (add to desc?)
+                Generic.curProtocol.outPRVMSG(this, replyto, "\u0002Error:\u0002 Too many arguments!");
+                return;
+            }
+
+            // Is there a service that provides access levels?
+            if (Configuration.getSvc().containsKey(Configuration.authservice)) {
+
+                CommandDesc.access_levels req = Commands.get(cmd.toLowerCase()).getDesc().Required_Access;
+                String handle = Generic.Users.get(user.toLowerCase()).authhandle;
+
+/*  // I think at this point if there is a nonexistant user, we have bigger issues
+                if (Generic.Users.containsKey(user.toLowerCase()))
+*/
+
+                CommandDesc.access_levels cur = ((SrvAuth) Configuration.getSvc().get(Configuration.authservice)).checkAccess(handle);
+                // Do we need any access to use this command?
+                if ((handle == null) && (req.ordinal() != 0)) {
+                    Generic.curProtocol.outPRVMSG(this, replyto, "\u0002Error:\u0002 You're not authed!");
+                    return;
+                }
+
+                // Do we have enough access?
+                if (cur.ordinal() < req.ordinal()) {
+                    Generic.curProtocol.outPRVMSG(this, replyto, "\u0002Error:\u0002 Not enough access!");
+                    return;
+                }
+
+                // All checks passed, you are clear for takeoff
+                Logging.verbose("SERVICE", "Handling command: " + cmd + ", for user: " + user + ".");
+                Commands.get(cmd.toLowerCase()).handle_command(this, user.toLowerCase(), replyto, command.substring(cmd.length()).trim()); // run command
+
+
+            } else {
+                // there isn't, just pass the command
+                Commands.get(cmd.toLowerCase()).handle_command(this, user, replyto, command.substring(cmd.length()).trim());
+            }
+
+        } catch (NullPointerException NPE) {
+            // God damnit, what broke?
             Logging.error("SERVICE", "Caught NPE in command.");
             Logging.info("SERVICE", "NPE handling command:" + user + " \\ " + replyto + " \\ " + command);
             NPE.printStackTrace();
@@ -97,16 +128,17 @@ public abstract class bService implements Service {
     public abstract String getSRVDB();
 
     public abstract void loadSRVDB(NodeList XMLin);
+
     public void hookDispatch(Service which, Hooks.Events what, String source, String target, String args) {
         switch (what) {
             case E_PRIVMSG:
                 // todo: fix this up so it's not such a steaming pile of crap
                 String target2 = "", arguments = "";
 
-                 if (target.startsWith("#")) { // is a channel message
+                if (target.startsWith("#")) { // is a channel message
                     if (args.split(" ")[0].endsWith(":")) { // someone is addressing something. ie blah:
                         target2 = args.split(" ")[0];
-                        target2 = target2.substring(0,target2.length()-1);
+                        target2 = target2.substring(0, target2.length() - 1);
                         arguments = args.substring(target2.length() + 1).trim();
                     }
                 } else { // is a private message
@@ -121,9 +153,9 @@ public abstract class bService implements Service {
                 if (temp.length > 1)
                     arguments = "FQDN" + arguments;
                 if (target2.toLowerCase().equals(which.getname().toLowerCase()))
-                which.handle(
+                    which.handle(
                             source,
-                            (target.startsWith("#")?target:source),
+                            (target.startsWith("#") ? target : source),
                             arguments
                     );
 
@@ -131,5 +163,5 @@ public abstract class bService implements Service {
             default:
         }
     }
-    
+
 }
