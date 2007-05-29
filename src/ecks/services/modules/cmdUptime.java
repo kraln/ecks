c69e89d6f318b9ed8722265bf90bd83d54f6bcbf
TehKuja <TehKuja@2e3bee3b-6622-0410-b2a2-7f6c7436e678>
2007-05-29 10:03:53 +0000

    Minor fixes to uptime command and storage...

--- src/ecks/services/modules/cmdUptime.java
+++ src/ecks/services/modules/cmdUptime.java
@@ -33,7 +33,18 @@ public class cmdUptime extends bCommand {
     }
 
     public void handle_command(Service who, String user, String replyto, String arguments) {
-    DateFormat mySimpleDateFormat = new SimpleDateFormat( "HH:MM SS" );
-        Generic.curProtocol.outPRVMSG(who, replyto, "\u0002" + Generic.Users.get(user).uid + ":\u0002 Ecks " + util.getVersion() + " has been up for " + mySimpleDateFormat.format( new Date((Long.valueOf(util.getTS()) - Generic.curProtocol.getWhenStarted()) * 1000 )) + ", with " + util.getThreads().size() + " threads");
+
+    long started = Long.valueOf(util.getTS()) - Generic.curProtocol.getWhenStarted();
+    long days =  started / 86400;
+    long hours = (started - (days * 86400)) / 3600;
+    long minutes = (started - (days * 86400) - (hours * 3600)) / 60 ;
+
+    String uptime = ((days>0)?days + " day" + (days>1?"s":"") + "," :"") + ((hours>0)?hours + " hour"+(hours>1?"s":"")+", ":"") + ((minutes>0)?((days>0 || hours>0)?" and ":"") + minutes + " minute" + (minutes>1?"s":""):"");
+        if (uptime.equals(""))
+        {
+            Generic.curProtocol.outPRVMSG(who, replyto, "\u0002" + Generic.Users.get(user).uid + ":\u0002 Ecks " + util.getVersion() + " has been up for " + started + " second"+(started!=1?"s":"")+", with " + util.getThreads().size() + " threads using " + ((Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory())/1024) + "kb of memory");
+        } else {
+            Generic.curProtocol.outPRVMSG(who, replyto, "\u0002" + Generic.Users.get(user).uid + ":\u0002 Ecks " + util.getVersion() + " has been up for " + uptime + ", with " + util.getThreads().size() + " threads using " + ((Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory())/1024) + "kb of memory");
+        }
     }
 }
